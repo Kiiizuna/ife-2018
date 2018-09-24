b54f40a9c451da56ad27c34358b6c7456bb0d9e5
Kiiizuna <936511537@qq.com>
2018-09-24 14:23:22 +0800

    update list

--- zero basic/28-30-email-sug/main.js
+++ zero basic/28-30-email-sug/main.js
@@ -3,17 +3,19 @@ const log = function() {
 }
 
 const inputDom的输入监听 = function() {
-    var val = ''
+    var value = ''
+    var li = ''
+    // var list = []
     // 获取用户输入
     getInput()
     // 生成提示框中的提示内容
     // 将提示内容添加到email-sug-wrapper中
     // 控制email-sug-wrapper的显示/隐藏状态
 }
-
 inputDom的输入监听()
 
 function getInput() {
+    // var val = ''
 	  // 获取用户输入()
     // 拿到input输入框的输入内容trim后返回
     var emailInput = document.querySelector('#email-input')
@@ -23,6 +25,7 @@ function getInput() {
       var rawValue = emailInput.value
       val = rawValue.trim()
       log('val is', val)
+      生成提示框中的提示内容()
    })
    //  emailInput.addEventListener('keypress',function(){
    // 		log('keypress')
@@ -38,7 +41,21 @@ function getInput() {
 function 生成提示框中的提示内容() {
     // 获取用户输入
     var postfixList = ['163.com', 'gmail.com', '126.com', 'qq.com', '263.net'];
-    
+    var len = postfixList.length
+    var l = ''
+    var list = []
+    for (var i = 0; i < len; i++) {
+        l = val + '@' + postfixList[i]
+        li = `<li>${l}</li>`
+        log('li is ', li)
+        list.push(li)
+        log('list arr ', list)
+        // 将提示内容添加到ul中()
+    }
+    var ulWrapper = document.querySelector('#email-sug-wrapper')
+    for (var i = 0; i < list.length; i++) {
+      ulWrapper.insertAdjacentHTML('beforeend', list[i])   
+    }
     // 遍历postfixList {
     //     把用户输入和每一个postfix进行结合成为每一个Li
     // }
@@ -46,6 +63,8 @@ function 生成提示框中的提示内容() {
 }
 
 function 将提示内容添加到ul中() {
+    var ulWrapper = document.querySelector('#email-sug-wrapper')
+    ulWrapper.insertAdjacentHTML('beforeend', list) 
     // email-sug-wrapper
     // 获取生成提示框中的提示内容
     // 将内容添加到email-sug-wrapper中
