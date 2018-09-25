7341f580057097a9b1d021077285c1362c12642a
Kiiizuna <936511537@qq.com>
2018-09-25 22:13:07 +0800

    update match select

--- zero basic/28-30-email-sug/main.js
+++ zero basic/28-30-email-sug/main.js
@@ -5,6 +5,7 @@ const log = function() {
 const inputDom的输入监听 = function() {
     var li = ''
     var val = ''
+    var val2 = ''
     // var list = []
     // 获取用户输入
     getInput()
@@ -26,8 +27,10 @@ function getInput() {
       val = rawValue.trim()
       if (val.indexOf('@') > -1) {
         var index = val.indexOf('@')
+        val2 = val.slice(index + 1)
         val = val.slice(0, index)
         log('val is', val)
+        log('val2 is', val2)
         生成提示框中的提示内容()
         控制ul的显示或者隐藏状态()
       } else {
@@ -46,6 +49,18 @@ function getInput() {
    // 		log('oninput')
    // })
 }
+// function 生成提示框中的提示内容() {
+//     用来拼接的用户输入内容 = 获取用户输入
+//     if 用户输入含有@ {
+//         用来拼接的用户输入内容 = @之前的字符串
+//         用来前缀匹配的用户输入内容 = @之后的字符串
+//     }    
+//     遍历postfixList {
+//         if 用来前缀匹配的用户输入内容前缀匹配遍历字符串元素
+//             把用来拼接的用户输入内容和这个字符串进行结合成为一个Li
+//     }
+//     返回生成的提示内容
+// }
 
 function 生成提示框中的提示内容() {
     // 获取用户输入
@@ -53,19 +68,40 @@ function 生成提示框中的提示内容() {
     var len = postfixList.length
     var l = ''
     var list = []
+    var list2 = []
     for (var i = 0; i < len; i++) {
-        l = val + '@' + postfixList[i]
-        li = `<li>${l}</li>`
-        log('li is ', li)
-        list.push(li)
-        log('list arr ', list)
+        if (postfixList[i].indexOf(val2) > -1) {
+          l = val + '@' + postfixList[i]
+          li = `<li>${l}</li>`
+          log('li is8888 ', li)
+          list.push(li)
+          log('list arr8888 ', list)
+        } else {
+          l = val + '@' + postfixList[i]
+          li = `<li>${l}</li>`
+          log('li is444 ', li)
+          list2.push(li)
+          log('list arr444 ', list2)
+        }
+        
         // 将提示内容添加到ul中()
     }
-    var ulWrapper = document.querySelector('#email-sug-wrapper')
-    ulWrapper.innerHTML = ''
-    for (var i = 0; i < list.length; i++) {
-      ulWrapper.insertAdjacentHTML('beforeend', list[i])   
+    if (list.length != 0) {
+        var ulWrapper = document.querySelector('#email-sug-wrapper')
+        ulWrapper.innerHTML = ''
+        // 在for 循环逐条插入前先清0
+        for (var i = 0; i < list.length; i++) {
+        ulWrapper.insertAdjacentHTML('beforeend', list[i])   
+      }
+    } else {
+        var ulWrapper = document.querySelector('#email-sug-wrapper')
+        ulWrapper.innerHTML = ''
+        // 在for 循环逐条插入前先清0
+        for (var i = 0; i < list2.length; i++) {
+        ulWrapper.insertAdjacentHTML('beforeend', list2[i])
+      }  
     }
+   
     // 将提示内容添加到ul中() 
     // 遍历postfixList {
     //     把用户输入和每一个postfix进行结合成为每一个Li
